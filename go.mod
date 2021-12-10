6a1550789f4a16f8e23cda7d6fc60fed4cd74aa6
Lucian I. Last <li@last.nl>
2021-12-10 17:47:44 +0100

    Improve cli

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/stn1slv/http-proxy-logger
 
 go 1.16
+
+require github.com/urfave/cli/v2 v2.3.0
--- main.go
+++ main.go
@@ -1,13 +1,58 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
 	"os"
 	"sync/atomic"
+
+	"github.com/urfave/cli/v2"
 )
+
+func main() {
+	app := &cli.App{
+		Name:  "http-proxy-logger",
+		Usage: "This is HTTP proxy which prints http requests and responses to console including http body.",
+		Flags: []cli.Flag{
+			&cli.IntFlag{
+				Name:     "port",
+				Aliases:  []string{"p", "listen-port"},
+				Required: true,
+				Usage:    "Listening port number",
+			},
+			&cli.StringFlag{
+				Name:     "target-address",
+				Aliases:  []string{"t", "target"},
+				Usage:    "Proxy target address",
+				Required: true,
+			},
+		},
+		Action: func(c *cli.Context) error {
+			port := c.Int("port")
+			target :=
+				c.String("target")
+			targetURL, err := url.Parse(target)
+			if err != nil {
+				return err
+			}
+
+			Proxy(port, targetURL)
+
+			return nil
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// Proxy
+// --------------------------------
 
 // Request counter
 var reqCounter int32
@@ -37,30 +82,11 @@ func (DebugTransport) RoundTrip(r *http.Request) (*http.Response, error) {
 	return response, err
 }
 
-// Get env var or default
-func getEnv(key, fallback string) string {
-	if value, ok := os.LookupEnv(key); ok {
-		return value
-	}
-	return fallback
-}
+func Proxy(listenPort int, targetURL *url.URL) error {
+	listenAddr := fmt.Sprintf(":%d", listenPort)
+	log.Printf("Forwarding %s -> %s\n", listenAddr, targetURL.String())
 
-// Get the port to listen on
-func getListenAddress() string {
-	port := getEnv("PORT", "1338")
-	return ":" + port
-}
-
-func getTarget() string {
-	target := getEnv("TARGET", "http://example.com")
-	return target
-}
-
-func main() {
-	target, _ := url.Parse(getTarget())
-	log.Printf("Forwarding %s -> %s\n", getListenAddress(), target)
-
-	proxy := httputil.NewSingleHostReverseProxy(target)
+	proxy := httputil.NewSingleHostReverseProxy(targetURL)
 
 	proxy.Transport = DebugTransport{}
 
@@ -68,10 +94,8 @@ func main() {
 	proxy.Director = func(r *http.Request) {
 		d(r) // call default director
 
-		r.Host = target.Host // set Host header as expected by target
+		r.Host = targetURL.Host // set Host header as expected by target
 	}
 
-	if err := http.ListenAndServe(getListenAddress(), proxy); err != nil {
-		panic(err)
-	}
+	return http.ListenAndServe(listenAddr, proxy)
 }
